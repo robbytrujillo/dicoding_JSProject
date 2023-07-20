134d723d790c48e996f57f3a809e5e46ac9edb88
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-07-20 09:47:43 +0700

    response body

---
+++ responseBody/responseBody.js
@@ -0,0 +1,9 @@
+const requestListener = (request, response) => {
+    response.write('<html>');
+    response.write('<body>');
+    response.write('<h1>Hello, World!</h1>');
+    response.write('</body>');
+    response.write('</html>');
+    response.end();
+    response.end('<html><body><h1>Hello, World</h1></body></html>')
+}
